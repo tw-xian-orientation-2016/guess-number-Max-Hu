14cf61f2bfd8ef896e7c7a7c523ce812e9262f48
Max-Hu <huzhiwise@gmail.com>
2016-03-04 14:35:28 +0800

    change digit name

--- src/AnswerGenerator.java
+++ src/AnswerGenerator.java
@@ -12,8 +12,8 @@ public class AnswerGenerator {
     public String getAnswer(){
         String answer = "";
         while (answer.length() < 4){
-            String  digit = String.valueOf(random.nextInt(10));
-            if (answer.indexOf(digit) < 0) answer += digit;
+            String  current = String.valueOf(random.nextInt(10));
+            if (answer.indexOf(current) < 0) answer += current;
         }
         return answer;
     }
