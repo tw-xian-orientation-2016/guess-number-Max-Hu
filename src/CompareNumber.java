990e5ad03b0077a05a6730f48f3f97d14ef0bc4e
Max-Hu <huzhiwise@gmail.com>
2016-03-04 11:10:27 +0800

    cretate first task

--- src/CompareNumber.java
+++ src/CompareNumber.java
@@ -1,2 +1,17 @@
 public class CompareNumber {
+    public String compare(String answer,String input) {
+        int aNumber = 0;
+        int bNumber = 0;
+        String inputArr[] = input.split("");
+        String answerArr[] = answer.split("");
+        for (int a = 0; a < inputArr.length; a++) {
+            for (int b = 0; b < answerArr.length; b++) {
+                if (inputArr[a].equals(answerArr[b])) {
+                    if (a == b) aNumber++;
+                    else bNumber ++;
+                }
+            }
+        }
+        return aNumber + "A" + bNumber + "B";
+    }
 }
--- test/CompareNumberTest.java
+++ test/CompareNumberTest.java
@@ -1,2 +1,44 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CompareNumberTest {
+
+    CompareNumber compareNumber = new CompareNumber();
+
+    @Test
+    public void VerifyOutPutIsFourAOB(){
+        String input = "1234";
+        String answer = "1234";
+        String result = compareNumber.compare(answer,input);
+        assertEquals("4A0B",result);
+    }
+
+    @Test
+    public void VerifyOutPutIsOAFourB(){
+        String input = "1234";
+        String answer = "4321";
+        String result = compareNumber.compare(answer,input);
+        assertEquals("0A4B",result);
+    }
+
+    @Test
+    public void VerifyOutPutIsOAOB(){
+        String input = "1234";
+        String answer = "5678";
+        String result = compareNumber.compare(answer,input);
+        assertEquals("0A0B",result);
+    }
+
+    @Test
+    public void VerifyOutPutIsNotCorrect(){
+        String input = "1234";
+        String answer = "5678";
+        String result = compareNumber.compare(answer,input);
+        assertNotEquals("1A3B",result);
+    }
+
+
+
+
 }
